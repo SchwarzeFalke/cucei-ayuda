ba33bb2c86b2a6b522da53bfc969509f7697ab4e
Brandon Diaz <brandonmanuel@live.com.mx>
2018-09-13 20:00:28 -0500

    #4 desarrollando primeras rutas

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-/**
- * @Author: schwarze_falke
- * @Date:   2018-09-13T13:50:03-05:00
- * @Last modified by:   schwarze_falke
- * @Last modified time: 2018-09-13T14:17:34-05:00
- */
 
 const express = require('express');
 
 const app = express();
+
+
+
+app.get('/forum/post')
 
 // app.get('/users', (req, res) => {
 //   const users = [
