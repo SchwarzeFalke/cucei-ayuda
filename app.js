4ed870401c8cd8ba749af6eac63bd7f342b3c800
Tyler Ruiz <tyler.ruiz@alumnos.udg.mx>
2018-09-23 20:13:12 -0500

    Adding models and controller

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 /**
  * @Author: schwarze_falke
- * @Date:   2018-09-13T13:50:03-05:00
- * @Last modified by:   root
- * @Last modified time: 2018-09-19T02:41:31-05:00
-*/
+ * @Date:   2018-09-21T19:19:18-05:00
+ * @Last modified by:   schwarze_falke
+ * @Last modified time: 2018-09-21T19:56:36-05:00
+ */
+
+require('dotenv').config();
+
 const express = require('express');
 
+
 const app = express();
+
 const router = require('./routes');
 
 app.use(router);
 
-app.listen(3000, () => console.log('My cute app is running on port 3000!'));
+app.listen(process.env.PORT, () => console.log('My cute app is running on port 3000!'));
