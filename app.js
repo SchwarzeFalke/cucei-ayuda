4f863c12f3910af207ae77e4ae3df8e2c997829e
SchwarzeFalke <autor.cvp303@gmail.com>
2018-10-08 03:10:24 -0500

    .

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
  * @Author: schwarze_falke
  * @Date:   2018-09-21T19:19:18-05:00
  * @Last modified by:   schwarze_falke
- * @Last modified time: 2018-10-08T03:08:16-05:00
+ * @Last modified time: 2018-10-08T03:10:20-05:00
  */
 require('dotenv').config();
 
@@ -14,4 +14,4 @@ const router = require('./routes');
 
 app.use(router);
 
-app.listen(process.Getenv('PORT') || 5000);
+app.listen(process.env.PORT || 5000);
