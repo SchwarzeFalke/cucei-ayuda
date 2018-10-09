752c4602d6edf3a7e95ce69135a6f04e24a46784
SchwarzeFalke <autor.cvp303@gmail.com>
2018-10-09 05:37:11 -0500

    For god sake, work!

--- db/index.js
+++ db/index.js
@@ -2,23 +2,20 @@
  * @Author: schwarze_falke
  * @Date:   2018-10-08T14:34:11-05:00
  * @Last modified by:   schwarze_falke
- * @Last modified time: 2018-10-09T05:28:21-05:00
+ * @Last modified time: 2018-10-09T05:34:30-05:00
  */
 
 const mysql = require('mysql');
 
 class DB {
-  constructor() {
+  get(table, columns, condition, order) {
+    // this.connection.connect();
     this.connection = mysql.createConnection({
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
     });
-  }
-
-  get(table, columns, condition, order) {
-    // this.connection.connect();
     return new Promise((resolve, reject) => {
       // const data = [columns, table];
       let query = 'SELECT ?? FROM ??'; // avoid logical deleted data
@@ -55,6 +52,12 @@ class DB {
 
   update(table, data, condition) {
     // this.connection.connect();
+    this.connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    });
     return new Promise((resolve, reject) => {
       let query = 'UPDATE ?? SET ?';
       if (condition) query += ` WHERE ${condition} && exist = 1;`;
@@ -70,6 +73,12 @@ class DB {
 
   physicalDel(table, condition) {
     // this.connection.connect();
+    this.connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    });
     return new Promise((resolve, reject) => {
       let query = 'DELETE FROM ??';
       if (condition) query += ` WHERE ${condition};`;
@@ -84,6 +93,12 @@ class DB {
 
   logicalDel(table, condition) {
     // this.connection.connect();
+    this.connection = mysql.createConnection({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    });
     return new Promise((resolve, reject) => {
       let query = 'UPDATE ?? SET exist = 0';
       if (condition) query += ` WHERE ${condition}`;
@@ -94,10 +109,6 @@ class DB {
         resolve(results);
       });
     });
-  }
-
-  endConnection() {
-    this.connection.end();
   }
 }
 
