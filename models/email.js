8cb67b57a87c767d6a3845895e7ecacb555fafdd
SchwarzeFalke <autor.cvp303@gmail.com>
2018-10-11 02:47:06 -0500

    Creating email model

--- models/email.js
+++ models/email.js
@@ -2,38 +2,47 @@
  * @Author: schwarze_falke
  * @Date:   2018-10-09T07:49:32-05:00
  * @Last modified by:   schwarze_falke
- * @Last modified time: 2018-10-09T07:51:06-05:00
+ * @Last modified time: 2018-10-11T02:46:50-05:00
  */
 const db = require('../db'); // for database handling
 
-class RoadMdl {
-  static async getBuildings(column, condition) {
+class EmailMdl {
+  constructor(args) {
+    if (args.email !== undefined) this.email = args.email;
+    if (args.user_code !== undefined) this.user_code = args.user_code;
+  }
+
+  static async get(id) {
     return new Promise(async (resolve, reject) => {
-      this.information = [];
-      await db.get('subject_lists', column, condition)
-        .then(async (results) => {
-          for (const data of results) {
-            this.information.push(await db.get('subject', '*', `nrc = ${data.subject_id}`));
-          }
-          return resolve(this.information);
+      this.emails = [];
+      await db.get('email', '*', `WHERE user_code = ${id}`)
+        .then((result) => {
+          result.forEach((email) => {
+            this.emails.push(email);
+          });
+        })
+        .catch(e => reject(e));
+      return resolve(this.emails);
+    });
+  }
+
+  async insert(id, userEmail) {
+    return new Promise(async (resolve, reject) => {
+      this.information = {
+        user_code: id,
+        email: userEmail,
+      };
+      await db.insert('email', this.information)
+        .then((result) => {
+          this.reponse = {
+            dataCreated: this.information,
+            info: result,
+          };
+          return resolve(this.response);
         })
         .catch(e => reject(e));
     });
   }
+}
 
-   static async get(data) {
-     return new Promise(async (resolve, reject) => {
-       this.information = [];
-       for(const bld of data) {
-         await db.get('building', 'latitude, longitude', `building_id = ${bld[0].building}`)
-           .then((result) => {
-             this.information.push(result);
-           })
-           .catch(e => reject(e));
-       }
-       return resolve(this.information);
-     });
-   }
- }
-
- module.exports = RoadMdl;
+module.exports = EmailMdl;
