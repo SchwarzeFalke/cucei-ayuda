acf4e4f47d20b07135f13748ccce4f29238fa70e
SchwarzeFalke <autor.cvp303@gmail.com>
2018-10-11 10:22:26 -0500

    Creating model methods for token (destroy and create)

--- models/token.js
+++ models/token.js
@@ -2,5 +2,36 @@
  * @Author: schwarze_falke
  * @Date:   2018-10-11T09:26:08-05:00
  * @Last modified by:   schwarze_falke
- * @Last modified time: 2018-10-11T09:26:09-05:00
+ * @Last modified time: 2018-10-11T10:21:28-05:00
  */
+
+const db = require('../db'); // for database handling
+
+class Token {
+  constructor(args) {
+    this.token = args.token;
+    this.created_at = args.created_at;
+    this.duration = args.duration;
+    this.type = args.type;
+    this.active = args.active;
+    this.user_id = args.suser.id;
+  }
+
+  async create() {
+    await db.insert('token', this)
+      .then(() => this.token)
+      .catch(e => console.error(`.catch(${e})`));
+  }
+
+  async destroy(token) {
+    await db.update('token', 'session(1)', `WHERE token = ${token}`)
+      .then((result) => {
+        if (result.affectedRows === 1) {
+          this.response = 'Successfully ended session';
+        } else { this.response = 'Cannot end session; token does not exist!'; }
+        return this.response;
+      });
+  }
+}
+
+module.exports = Token;
