6c8e297be6fbae037b9656e11d5b8e13aa27c092
Julio <julio.mariscal.bautista@gmail.com>
2018-10-28 18:29:50 -0600

    Se estandarizan las respuestas en buildings

--- controllers/building.js
+++ controllers/building.js
@@ -2,7 +2,7 @@
  * @author: JulioMariscal
  */
 
-const { BuildingMdl } = require('../models');
+const { BuildingMdl, ResMdl } = require('../models');
 
 // FIXME Todos los metodos deben estar documentados
 // FIXME En lugar de hacer los send de cada error, podria ser un next con error y
@@ -26,22 +26,24 @@ class BuildingCrtl {
      * @return {Promise}     [description]
      */
     async getAll(req, res) {
+        const newResponse = new ResMdl();
         try {
             await BuildingMdl.getAll()
                 .then((data) => {
                     if (data.length === 0) {
-                        res.status(this.noContentJSON.status).send(this.noContentJSON);
+                        newResponse.createResponse(data, 204, '/buildings', 'GET');
                     } else {
-                        console.log('You see all buildings');
-                        this.okJSON.message = 'You see all buildings';
-                        this.okJSON.data = data;
-                        res.status(this.okJSON.status).send(this.okJSON);
+                        newResponse.createResponse(data, 200, '/buildings', 'GET');
                     }
-                })
-                .catch(e => console.error(`We have a error!(${e})`));
+                });
+            newResponse.response.message = newResponse.createMessage();
+            this.response = newResponse;
+            res.status(this.response.response.status).send(this.response.response);
         } catch (e) {
-            res.status(this.forbiddenJSON.status).send(this.forbiddenJSON);
-            console.error(`We have a error!(${e})`);
+            newResponse.response('There is nothing to retrieve', 500, e, 'GET');
+            newResponse.response.message = newResponse.createMessage();
+            this.response = newResponse;
+            res.status(this.response.response.status).send(this.response.response);
         }
     }
 
@@ -52,25 +54,35 @@ class BuildingCrtl {
      * @return {Promise}     [description]
      */
     async getBuild(req, res) {
+        const newResponse = new ResMdl();
         try {
             await BuildingMdl.validBuilding(req.params.buildingId)
                 .then((exists) => {
                     if (exists) {
                         BuildingMdl.get('*', req.params.buildingId)
                             .then((data) => {
-                                this.okJSON.data = data;
-                                res.status(this.okJSON.status).send(this.okJSON);
-                            })
-                            .catch(e => console.error(`We have a error!(${e})`));
+                                if (data.length >= 1) {
+                                    newResponse.createResponse(data, 200, '/buildings', 'GET');
+                                } else {
+                                    newResponse.createResponse(data, 204, '/buildings', 'GET');
+                                }
+                                newResponse.response.message = newResponse.createMessage();
+                                this.response = newResponse;
+                                res.status(this.response.response.status)
+                                    .send(this.response.response);
+                            });
                     } else {
-                        this.notFoundJSON.message = 'The requested building dont exist';
-                        res.status(this.notFoundJSON.status).send(this.notFoundJSON);
+                        newResponse.createResponse('Nothing to show', 404, '/building', 'GET');
+                        newResponse.response.message = newResponse.createMessage();
+                        this.response = newResponse;
+                        res.status(this.response.response.status).send(this.response.response);
                     }
-                })
-                .catch(e => console.error(`We have a error!(${e})`));
+                });
         } catch (e) {
-            res.status(this.forbiddenJSON.status).send(this.forbiddenJSON);
-            console.error(`We have a error!(${e})`);
+            newResponse.response('There is nothing to retrieve', 500, e, 'GET');
+            newResponse.response.message = newResponse.createMessage();
+            this.response = newResponse;
+            res.status(this.response.response.status).send(this.response.response);
         }
     }
 
@@ -81,6 +93,7 @@ class BuildingCrtl {
      * @return {Promise}     [description]
      */
     async insert(req, res) {
+        const newResponse = new ResMdl();
         const newBuilding = new BuildingMdl({ ...req.body });
         try {
             await BuildingMdl.validBuilding(req.body.building_id)
@@ -88,23 +101,20 @@ class BuildingCrtl {
                     if (!exists) {
                         newBuilding.save()
                             .then((data) => {
-                                console.log(`Crated new building with id: ${req.body.building_id}`);
-                                this.info = data;
-                                this.createJSON.response = 'Created';
-                                this.createJSON.message = 'Created new building';
-                                this.createJSON.data = newBuilding;
-                                res.status(this.createJSON.status).send(this.createJSON);
-                            })
-                            .catch(e => console.error(`We have a error!(${e})`));
+                                newResponse.createResponse(data, 200, '/buildings', 'GET');
+                            });
                     } else {
-                        this.badRequestJSON.message = 'we have a building with this id';
-                        res.status(this.badRequestJSON.status).send(this.badRequestJSON);
+                        newResponse.createResponse('Nothing to show', 400, '/buildings', 'GET');
                     }
-                })
-                .catch(e => console.error(`We have a error!(${e})`));
+                });
+            newResponse.response.message = newResponse.createMessage();
+            this.response = newResponse;
+            res.status(this.response.response.status).send(this.response.response);
         } catch (e) {
-            res.status(this.badRequestJSON.status).send(this.badRequestJSON);
-            console.error(`We have a error!(${e})`);
+            newResponse.response('There is nothing to retrieve', 500, e, 'GET');
+            newResponse.response.message = newResponse.createMessage();
+            this.response = newResponse;
+            res.status(this.response.response.status).send(this.response.response);
         }
     }
 
@@ -115,6 +125,7 @@ class BuildingCrtl {
      * @return {Promise}     [description]
      */
     async modify(req, res) {
+        const newResponse = new ResMdl();
         const modifyBuilding = new BuildingMdl({ ...req.body });
         try {
             await BuildingMdl.validBuilding(req.params.buildingId)
@@ -122,22 +133,20 @@ class BuildingCrtl {
                     if (exists) {
                         modifyBuilding.update(req.params.buildingId)
                             .then((data) => {
-                                console.log('You modify a building');
-                                this.okJSON.response = 'Updated';
-                                this.okJSON.message = 'You modify a building';
-                                this.okJSON.data = data;
-                                res.status(this.okJSON.status).send(this.okJSON);
-                            })
-                            .catch(e => console.error(`We have a error!(${e})`));
+                                newResponse.createResponse(data, 200, '/buildings', 'GET');
+                            });
                     } else {
-                        this.notFoundJSON.message = 'The requested building dont exist';
-                        res.status(this.notFoundJSON.status).send(this.notFoundJSON);
+                        newResponse.createResponse('Nothing to show', 404, '/buildings', 'GET');
                     }
-                })
-                .catch(e => console.error(`We have a error!(${e})`));
+                });
+            newResponse.response.message = newResponse.createMessage();
+            this.response = newResponse;
+            res.status(this.response.response.status).send(this.response.response);
         } catch (e) {
-            res.status(this.forbiddenJSON.status).send(this.forbiddenJSON);
-            console.error(`We have a error!(${e})`);
+            newResponse.response('There is nothing to retrieve', 500, e, 'GET');
+            newResponse.response.message = newResponse.createMessage();
+            this.response = newResponse;
+            res.status(this.response.response.status).send(this.response.response);
         }
     }
 
@@ -148,6 +157,7 @@ class BuildingCrtl {
      * @return {Promise}     [description]
     */
     async logDel(req, res) {
+        const newResponse = new ResMdl();
         const updateBuilding = new BuildingMdl({ ...req.body });
         try {
             await BuildingMdl.validBuilding(req.params.buildingId)
@@ -155,22 +165,20 @@ class BuildingCrtl {
                     if (exists) {
                         updateBuilding.logicalDel(req.params.buildingId)
                             .then((data) => {
-                                console.log('You delete a building');
-                                this.okJSON.response = 'Deleted';
-                                this.okJSON.message = 'You delete a building';
-                                this.okJSON.data = data;
-                                res.status(this.okJSON.status).send(this.okJSON);
-                            })
-                            .catch(e => console.error(`We have a error!(${e})`));
+                                newResponse.createResponse(data, 200, '/buildings', 'GET');
+                            });
                     } else {
-                        this.notFoundJSON.message = 'The requested building dont exist';
-                        res.status(this.notFoundJSON.status).send(this.notFoundJSON);
+                        newResponse.createResponse('Nothing to show', 404, '/buildings', 'GET');
                     }
-                })
-                .catch(e => console.error(`We have a error!(${e})`));
+                });
+            newResponse.response.message = newResponse.createMessage();
+            this.response = newResponse;
+            res.status(this.response.response.status).send(this.response.response);
         } catch (e) {
-            res.status(this.forbiddenJSON.status).send(this.forbiddenJSON);
-            console.error(`We have a error!(${e})`);
+            newResponse.response('There is nothing to retrieve', 500, e, 'GET');
+            newResponse.response.message = newResponse.createMessage();
+            this.response = newResponse;
+            res.status(this.response.response.status).send(this.response.response);
         }
     }
 }
