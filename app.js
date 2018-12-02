a8ebf59a26547b4de4167a9c162a0c4ba29c9fe6
Brandon Diaz <brandonmanuel@live.com.mx>
2018-12-01 19:23:25 -0600

    cores in app.js

--- app.js
+++ app.js
@@ -12,7 +12,19 @@ const express = require('express');
 const app = express();
 
 const router = require('./routes');
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(router);
 
 app.listen(process.env.PORT, () => console.log(`CUCEI AYUDA IS RUNNING ON PORT ${process.env.PORT}!`));
